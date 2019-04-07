a2aadc3fdd95979d6e675c6369beb7069d1c9b9e
Courtney Fay <fay.courtney@gmail.com>
2019-04-07 11:19:11 -0600

    CORS middleware for heroku

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+//middleware to get heroku working
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://boiling-fjord-16588.herokuapp.com/"
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization" //X-Requested-With,content-type
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  return next();
+});
+
 app.use("/", routes);
 
 app.listen(port, () => {
