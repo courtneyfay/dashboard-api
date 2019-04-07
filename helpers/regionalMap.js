712734092e757e9c5f534ad3341607f90014e5fe
Courtney Fay <fay.courtney@gmail.com>
2019-04-07 09:02:15 -0600

    formatting data properly for frontend consumption and mapping east asian and pacific countries in regionalMap. adding indicatorMap

---
+++ helpers/regionalMap.js
@@ -0,0 +1,17 @@
+const regionalMap = {
+  //east asia & pacific
+  TEA:
+    "'ASM','AUS','BGD','BRN','BTN','CHN','FJI','FSM','GUM','HKG','IDN','IND','JPN','KHM','KIR','KOR','LAO','MAC','MDV','MHL','MMR','MNG','MYS','NPL','NRU','NZL','PHL','PLW','PNG','PRK','PYF','SGP','SLB','THA','TLS','TUV','VNM','VUT','WSM'",
+  //europe & central asia
+  TEC: "",
+  //latin america & caribbean
+  TLA: "",
+  //middle east & north africa
+  TMN: "",
+  //south asia
+  TSA: "",
+  //sub-saharan africa
+  TSS: "'ERI', 'SOM', 'SSD'"
+};
+
+module.exports = regionalMap;
--- services.js
+++ services.js
@@ -1,18 +1,62 @@
 const { BigQuery } = require("@google-cloud/bigquery");
 const bigqueryClient = new BigQuery();
-const regionalMap = {
-  //east asia & pacific
-  TEA: "'PRK', 'HKG'",
-  //europe & central asia
-  TEC: "",
-  //latin america & caribbean
-  TLA: "",
-  //middle east & north africa
-  TMN: "",
-  //south asia
-  TSA: "",
-  //sub-saharan africa
-  TSS: ""
+const regionalMap = require("./helpers/regionalMap");
+const {
+  AIR_POLLUTION,
+  DISEASE,
+  HYGIENE,
+  LIFE_EXPECTANCY,
+  POISONING,
+  SUICIDE
+} = require("./helpers/indicatorMap");
+
+const _mapData = rows => {
+  let finalData = {};
+
+  rows.forEach(row => {
+    const value = row["value"];
+    const indicatorCode = row["indicator_code"];
+    const code = row["country_code"];
+    const name = row["country_name"];
+    const data = {
+      [code]: {
+        name
+      }
+    };
+    let countryObject = data[code];
+
+    //if country data doesn't yet exist in finalData, add it
+    if (!finalData[code]) {
+      finalData = Object.assign(finalData, data);
+    }
+
+    //add data points to country object
+    switch (indicatorCode) {
+      case AIR_POLLUTION:
+        countryObject["airPollution"] = value;
+        break;
+      case DISEASE:
+        countryObject["disease"] = value;
+        break;
+      case HYGIENE:
+        countryObject["hygiene"] = value;
+        break;
+      case LIFE_EXPECTANCY:
+        countryObject["lifeExpectancy"] = value;
+        break;
+      case POISONING:
+        countryObject["poisoning"] = value;
+        break;
+      case SUICIDE:
+        countryObject["suicide"] = value;
+        break;
+    }
+
+    //add updated countryObject to finalData for that country
+    finalData[code] = Object.assign(finalData[code], countryObject);
+  });
+
+  return finalData;
 };
 
 const mortalityData = async region => {
@@ -21,14 +65,13 @@ const mortalityData = async region => {
     const sqlQuery = `SELECT 
         *
     FROM \`bigquery-public-data.world_bank_health_population.health_nutrition_population\`
-    WHERE indicator_code in (
-    'SP.DYN.LE00.IN', 
-    'SH.DYN.NCOM.ZS',
-    'SH.STA.AIRP.P5',
-    'SH.STA.POIS.P5',
-    'SH.STA.WASH.P5',
-    'SH.STA.SUIC.P5'
-    )
+    WHERE indicator_code in 
+      ('${AIR_POLLUTION}',
+         '${DISEASE}',
+         '${HYGIENE}',
+         '${LIFE_EXPECTANCY}',
+         '${POISONING}',
+         '${SUICIDE}')
     and country_code in (${countries})
     and year = 2016
     order by country_code;`;
@@ -40,7 +83,7 @@ const mortalityData = async region => {
 
     const [rows] = await bigqueryClient.query(options);
 
-    return rows;
+    return _mapData(rows);
   } catch (err) {
     throw err;
   }
