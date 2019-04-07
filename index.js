7e796a52b3c8329109029a02ef36e42d3ac14326
Courtney Fay <fay.courtney@gmail.com>
2019-04-07 11:12:53 -0600

    still testing out heroku build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const routes = require("./routes.js");
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
