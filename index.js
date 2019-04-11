2ecfbb4deb00b532af911833026f6f2c3e1db10d
Courtney Fay <fay.courtney@gmail.com>
2019-04-10 20:40:28 -0600

    adding cors npm package instead of hardcoded middleware

--- index.js
+++ index.js
@@ -2,34 +2,26 @@ const express = require("express");
 const app = express();
 const routes = require("./routes.js");
 const port = process.env.PORT || 3000;
-
-app.use("/", routes);
+const cors = require("cors");
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Origin not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200
+};
 
 // Add CORS middleware headers
-app.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+app.use(cors(corsOptions));
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, OPTIONS" //POST, PUT, PATCH, DELETE
-  );
+// Add all routes
+app.use("/", routes);
 
-  // Request headers you wish to allow
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "X-Requested-With,content-type"
-  // );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
+// Start the server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
