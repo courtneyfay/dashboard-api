2165776f889cbfe1c315e8972ea6a32fcc0654bc
Courtney Fay <fay.courtney@gmail.com>
2019-04-10 20:43:13 -0600

    changing port number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const routes = require("./routes.js");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-const whitelist = ["http://localhost:3000"];
+const whitelist = ["http://localhost:8080"];
 const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
