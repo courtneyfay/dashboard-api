7a1958067b9c2cb0bc2f55e7e39d20d90dd3c03a
Courtney Fay <fay.courtney@gmail.com>
2019-04-13 08:19:30 -0600

    adding life expectancy data for line chart use on front end

--- services/health-mortality.js
+++ services/health-mortality.js
@@ -11,7 +11,40 @@ const { BigQuery } = require("@google-cloud/bigquery");
 const bigQueryClient = new BigQuery();
 const authenticateBigQuery = require("./authenticate");
 
-const _mapData = rows => {
+const _addLifeExpectancyData = (rows, mortalityData) => {
+  rows.forEach(row => {
+    const value = row.value;
+    const code = row.country_code;
+    const year = row.year;
+    const date = `${year}-01-01`;
+    let lineChart = {};
+
+    //if country code doesn't already exist in mortalityData, ignore it
+    if (!mortalityData[code]) {
+      return;
+    }
+    let countryObject = mortalityData[code];
+
+    //if lineChart object doesn't yet exist on mortalityData.countryObject, add it
+    if (!countryObject["lineChart"]) {
+      countryObject["lineChart"] = lineChart;
+    }
+
+    //if year doesn't yet exist in lineChart, add it
+    //then add new lineChart to countryObject
+    if (!lineChart[date]) {
+      lineChart[date] = value;
+      countryObject.lineChart = Object.assign(
+        countryObject.lineChart,
+        lineChart
+      );
+    }
+  });
+
+  return mortalityData;
+};
+
+const _mapMortalityData = rows => {
   let finalData = {};
 
   rows.forEach(row => {
@@ -60,8 +93,21 @@ const _mapData = rows => {
   return finalData;
 };
 
-const _buildSqlQuery = region => {
-  const countries = regionalMap[region];
+const _buildLifeExpectancyQuery = countries => {
+  const sqlQuery = `SELECT
+    country_code, year, value
+  FROM \`bigquery-public-data.world_bank_health_population.health_nutrition_population\`
+  WHERE indicator_code = '${LIFE_EXPECTANCY}'
+  and country_code in (${countries})
+  order by country_code;`;
+
+  return {
+    query: sqlQuery,
+    location: "US"
+  };
+};
+
+const _buildMortalityQuery = countries => {
   const sqlQuery = `SELECT 
       *
     FROM \`bigquery-public-data.world_bank_health_population.health_nutrition_population\`
@@ -83,13 +129,24 @@ const _buildSqlQuery = region => {
 };
 
 const getMortalityData = async region => {
+  //authenticate Big Query API
   await authenticateBigQuery(bigQueryClient);
-  const sqlQuery = _buildSqlQuery(region);
+
+  //build up both SQL queries for selected countries
+  const countries = regionalMap[region];
+  const mortalitySQL = _buildMortalityQuery(countries);
+  const lifeExpectancySQL = _buildLifeExpectancyQuery(countries);
 
   try {
-    const [rows] = await bigQueryClient.query(sqlQuery);
+    //make SQL requests for general mortality data for 2016
+    //and detailed life expectancy data from 1960 - 2016
+    const [rawMortalityData] = await bigQueryClient.query(mortalitySQL);
+    const [rawLifeExpectancyData] = await bigQueryClient.query(
+      lifeExpectancySQL
+    );
+    const mortalityData = _mapMortalityData(rawMortalityData);
 
-    return _mapData(rows);
+    return _addLifeExpectancyData(rawLifeExpectancyData, mortalityData);
   } catch (err) {
     throw err;
   }
